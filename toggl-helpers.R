13f61513d97e8ffe2e2f1972b922010d94a65b3c
Bosse Nyström <bon@praqma.net>
2019-01-04 12:57:04 +0100

    Add a helper file for the functions

---
+++ toggl-helpers.R
@@ -0,0 +1,77 @@
+if(Sys.getenv("TOGGL_TOKEN") == "" || Sys.getenv("TOGGL_WORKSPACE") == "") {
+  stop("Missing Environment Variables: TOGGL_TOKEN or TOGGL_WORKSPACE")
+}
+
+togglToken <- Sys.getenv("TOGGL_TOKEN")
+togglWid   <- Sys.getenv("TOGGL_WORKSPACE")
+
+getClients <- function(token, wid, verbose = FALSE) {
+  username <- token
+  password <- "api_token"
+
+
+  base <- "https://toggl.com/api"
+  endpoint <- "v8/workspaces"
+  what <- "clients"
+
+  call <- paste(base,endpoint,wid,what, sep="/")
+
+  if(verbose) {
+    result <- GET(call, authenticate(username,password), verbose())
+  } else {
+    result <- GET(call, authenticate(username,password))
+  }
+  return(result)
+}
+
+getProjects <- function(token, cid, verbose = FALSE) {
+  username <- token
+  password <- "api_token"
+
+
+  base <- "https://toggl.com/api"
+  endpoint <- "v8/clients"
+  what <- "projects"
+
+  call <- paste(base,endpoint,cid,what, sep="/")
+
+  if(verbose) {
+    result <- GET(call, authenticate(username,password), verbose())
+  } else {
+    result <- GET(call, authenticate(username,password))
+  }
+
+  return(result)
+}
+
+addStyle <- function(input) {
+
+  data <- input
+  data$style <- as.character(as.numeric(!data$billable))
+
+  return(data)
+}
+
+projectPlot <- function(data, name) {
+
+  data <- addStyle(data)
+
+  v1 <- ggplot(data=data) +
+    theme(axis.text.x = element_text(angle = 25, hjust = 1, size = 8)) +
+    labs(x=NULL, y="Hours", title=name, caption="data from toggl")
+
+  if ("actual_hours" %in% colnames(data)) {
+    v1 <- v1 + geom_col(aes(
+      x=name,
+      y=actual_hours,
+      fill=hex_color, linetype = style),
+      color = "Black",
+      show.legend = FALSE)
+  }
+  if ("estimated_hours" %in% colnames(data)) {
+    v1 <- v1 + geom_point(aes(x=name, y=estimated_hours),
+                          shape = 3, size = 2)
+  }
+
+  return(v1)
+}
--- toggl-projects.R
+++ toggl-projects.R
@@ -7,74 +7,7 @@ library(jsonlite)
 # install.package("httr")
 library(httr)
 
-if(Sys.getenv("TOGGL_TOKEN") == "" || Sys.getenv("TOGGL_WORKSPACE") == "") {
-  stop("Missing Environment Variables: TOGGL_TOKEN or TOGGL_WORKSPACE")
-}
-
-togglToken <- Sys.getenv("TOGGL_TOKEN")
-togglWid   <- Sys.getenv("TOGGL_WORKSPACE")
-
-
-getClients <- function(token, wid, verbose = FALSE) {
-  username <- token
-  password <- "api_token"
-
-
-  base <- "https://toggl.com/api"
-  endpoint <- "v8/workspaces"
-  what <- "clients"
-
-  call <- paste(base,endpoint,wid,what, sep="/")
-
-  if(verbose) {
-    result <- GET(call, authenticate(username,password), verbose())
-  } else {
-    result <- GET(call, authenticate(username,password))
-  }
-  return(result)
-}
-
-getProjects <- function(token, cid, verbose = FALSE) {
-  username <- token
-  password <- "api_token"
-
-
-  base <- "https://toggl.com/api"
-  endpoint <- "v8/clients"
-  what <- "projects"
-
-  call <- paste(base,endpoint,cid,what, sep="/")
-
-  if(verbose) {
-    result <- GET(call, authenticate(username,password), verbose())
-  } else {
-    result <- GET(call, authenticate(username,password))
-  }
-
-  return(result)
-}
-
-projectPlot <- function(data, name) {
-  # ToDo: check if data contains estimated_hours, colnames()
-  v1 <- ggplot(data=data) +
-    theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 8)) +
-    labs(x=NULL, y="Hours", title=name, caption="data from toggl")
-
-  if ("actual_hours" %in% colnames(data)) {
-    v1 <- v1 + geom_col(aes(
-      x=name,
-      y=actual_hours,
-      fill=hex_color),
-      color = "Black",
-      show.legend = FALSE)
-  }
-  if ("estimated_hours" %in% colnames(data)) {
-    v1 <- v1 + geom_point(aes(x=name, y=estimated_hours),
-                          shape = 3, size = 2)
-  }
-
-  return(v1)
-}
+source('toggl-helpers.R')
 
 clientsJSON <- getClients(togglToken, togglWid)
 
